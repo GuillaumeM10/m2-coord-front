41122c11b09a90c9bcdad0b21b9de548bb4a4107
vbetsch <vic.betsch@gmail.com>
2025-05-20 14:53:36 +0200

    feat: Implement table structure

--- src/app/pages/scoreboard-page/scoreboard-page.component.ts
+++ src/app/pages/scoreboard-page/scoreboard-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {scoreLinesMock} from '../../mocks/scoreboard/score-lines.mock';
+import {ScoreLineType} from '../../types/mocks/score-line.type';
 
 @Component({
   selector: 'app-scoreboard-page',
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef
+  ],
   templateUrl: './scoreboard-page.component.html',
   styleUrl: './scoreboard-page.component.scss',
 })
-export class ScoreboardPageComponent {}
+export class ScoreboardPageComponent {
+  displayedColumns: string[] = Object.keys(scoreLinesMock[0]);
+  dataSource: ScoreLineType[] = scoreLinesMock;
+}
---
+++ src/app/types/mocks/score-line.type.ts
@@ -0,0 +1,6 @@
+export type ScoreLineType = {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
