6488bfbf83bf43a746ae34e926c3eaab91c3d391
vbetsch <vic.betsch@gmail.com>
2025-05-20 14:06:23 +0200

    Revert "feat: Create home page"
    This reverts commit 8791b9b8794d0293a0c441bb501ba8dbd8025be2.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,4 @@ export const routes: Routes = [
     path: 'games',
     loadComponent: () => import('./pages/games/games.component').then(m => m.GamesComponent),
   },
-  {
-    path: '',
-    loadComponent: () => import('./pages/home-page/home-page.component').then(m => m.HomePageComponent),
-  }
 ];
