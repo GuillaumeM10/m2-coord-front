8791b9b8794d0293a0c441bb501ba8dbd8025be2
vbetsch <vic.betsch@gmail.com>
2025-05-20 12:40:03 +0200

    feat: Create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: 'games',
     loadComponent: () => import('./pages/games/games.component').then(m => m.GamesComponent),
   },
+  {
+    path: '',
+    loadComponent: () => import('./pages/home-page/home-page.component').then(m => m.HomePageComponent),
+  }
 ];
