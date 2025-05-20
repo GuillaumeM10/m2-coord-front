a89f559154b468ac95bc5f4fe83ddea20143341e
vbetsch <vic.betsch@gmail.com>
2025-05-20 16:35:43 +0200

    feat: Create DataColumn component

---
+++ src/app/components/common/tables/data-column/data-column.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DataColumnComponent } from './data-column.component';
+
+describe('DataColumnComponent', () => {
+  let component: DataColumnComponent;
+  let fixture: ComponentFixture<DataColumnComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DataColumnComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DataColumnComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/common/tables/data-column/data-column.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-column',
+  imports: [],
+  templateUrl: './data-column.component.html',
+  styleUrl: './data-column.component.scss'
+})
+export class DataColumnComponent {
+
+}
