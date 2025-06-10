650ccb12909792664851bfc9c42311e521bd26b8
vbetsch <vic.betsch@gmail.com>
2025-06-10 12:03:33 +0200

    tech: Disable @typescript-eslint/consistent-type-definitions ESLint rule

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
       "prettier/prettier": "error",
       "@angular-eslint/directive-selector": [
         "error",
