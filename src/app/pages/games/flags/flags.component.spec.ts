f98a7ebb53c2d825fcbede5dc3c2999c2a14610b
Augustin Colas <augus.colas@orange.fr>
2025-06-23 10:09:06 +0200

    fix: tests

--- src/app/pages/games/flags/flags.component.spec.ts
+++ src/app/pages/games/flags/flags.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FlagsComponent } from './flags.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { QuizzService } from '../../../services/quizz/quizz.service';
+import { of } from 'rxjs';
 
 describe('FlagsComponent', () => {
   let component: FlagsComponent;
@@ -8,7 +10,15 @@ describe('FlagsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FlagsComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: QuizzService,
+          useValue: {
+            getQuestions: () => of([]),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FlagsComponent);
