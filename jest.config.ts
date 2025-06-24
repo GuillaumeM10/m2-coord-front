71b61872c93a99958ee71429364baae8f2397033
Zephyor <nathan.mezou@outlook.fr>
2025-06-24 11:11:45 +0200

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   moduleNameMapper: {
     '^@api/(.*)$': '<rootDir>/src/api/$1',
     '^@app/(.*)$': '<rootDir>/src/app/$1',
