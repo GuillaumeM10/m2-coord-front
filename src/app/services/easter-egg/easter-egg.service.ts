d01c41453adb31c08a35eb296efadef8aaf0ef1a
vbetsch <vic.betsch@gmail.com>
2025-06-24 15:25:37 +0200

    refactor: Invert if

--- src/app/services/easter-egg/easter-egg.service.ts
+++ src/app/services/easter-egg/easter-egg.service.ts
@@ -10,14 +10,13 @@ export class EasterEggService {
   private readonly ctrlPressed$ = new BehaviorSubject<boolean>(false);
 
   constructor(@Inject(PLATFORM_ID) private readonly platformId: object) {
-    if (isPlatformBrowser(this.platformId)) {
-      window.addEventListener('keydown', e => {
-        if (e.key === EventKeyEnum.CONTROL) this.ctrlPressed$.next(true);
-      });
-      window.addEventListener('keyup', e => {
-        if (e.key === EventKeyEnum.CONTROL) this.ctrlPressed$.next(false);
-      });
-    }
+    if (!isPlatformBrowser(this.platformId)) return;
+    window.addEventListener('keydown', e => {
+      if (e.key === EventKeyEnum.CONTROL) this.ctrlPressed$.next(true);
+    });
+    window.addEventListener('keyup', e => {
+      if (e.key === EventKeyEnum.CONTROL) this.ctrlPressed$.next(false);
+    });
   }
 
   public isCtrlPressed(): Observable<boolean> {
