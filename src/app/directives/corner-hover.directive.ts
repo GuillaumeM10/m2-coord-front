d716460dfa6ffc0ec58f4ce5489d3523f7810c65
vbetsch <vic.betsch@gmail.com>
2025-06-24 14:49:33 +0200

    fix: Open only one modal

--- src/app/directives/corner-hover.directive.ts
+++ src/app/directives/corner-hover.directive.ts
@@ -1,14 +1,15 @@
-import {Directive, HostListener} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {EasterEggModalComponent} from '@app/components/modals/easter-egg-modal/easter-egg-modal.component';
-import {EasterEggService} from '@app/services/easter-egg/easter-egg.service';
-import {filter, take} from 'rxjs';
-import {EasterEggModalData} from '@app/components/modals/easter-egg-modal/easter-egg-modal-data.type';
+import { Directive, HostListener } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { EasterEggModalComponent } from '@app/components/modals/easter-egg-modal/easter-egg-modal.component';
+import { EasterEggService } from '@app/services/easter-egg/easter-egg.service';
+import { filter, take } from 'rxjs';
+import { EasterEggModalData } from '@app/components/modals/easter-egg-modal/easter-egg-modal-data.type';
 
 @Directive({
   selector: '[appCornerHover]',
 })
 export class CornerHoverDirective {
+  private modalIsOpen: boolean = false;
   private modalWidth: number = 400;
   private dataModal: EasterEggModalData = {
     image: 'rickroll.gif',
@@ -18,11 +19,10 @@ export class CornerHoverDirective {
   constructor(
     private easterEggService: EasterEggService,
     private dialog: MatDialog,
-  ) {
-  }
+  ) {}
 
   @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
+  private onMouseMove(event: MouseEvent): void {
     const inCorner =
       event.clientX > window.innerWidth * 0.95 && event.clientY < window.innerHeight * 0.05;
 
@@ -34,15 +34,29 @@ export class CornerHoverDirective {
           take(1),
         )
         .subscribe(() => {
-          this.openEasterEggModal();
+          this.openModalIfNot();
         });
     }
   }
 
-  openEasterEggModal() {
-    this.dialog.open(EasterEggModalComponent, {
-      width: this.modalWidth + 'px',
-      data: this.dataModal,
-    });
+  private openEasterEggModal(): MatDialogRef<EasterEggModalComponent> {
+    const modalRef: MatDialogRef<EasterEggModalComponent> = this.dialog.open(
+      EasterEggModalComponent,
+      {
+        width: this.modalWidth + 'px',
+        data: this.dataModal,
+      },
+    );
+    this.modalIsOpen = true;
+    return modalRef;
+  }
+
+  private openModalIfNot(): void {
+    if (!this.modalIsOpen) {
+      const modalRef: MatDialogRef<EasterEggModalComponent> = this.openEasterEggModal();
+      modalRef.afterClosed().subscribe(() => {
+        this.modalIsOpen = false;
+      });
+    }
   }
 }
